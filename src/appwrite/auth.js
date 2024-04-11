e525cca6bfba0a6d19f3f918e835c0d21c96009c
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-04-11 14:04:25 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,57 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl) // Your API Endpoint
+        .setProject(conf.appwriteProjectId);
+        this.account=new Account(this.client);
+    }
+
+    async createAccount({name,email,password}){
+        try{
+        const userAccount=await this.account.create(ID.unique(),email,password,name);
+        if(userAccount){
+
+        }
+        else{
+            return userAccount;
+        }
+    } catch(error){
+        throw error;
+    }
+    }
+
+    async Login({email,password}){
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService= new AuthService();
+
+export default authService
---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import SignUp from './SignUp'
+import Login from './Login'
+import RTE from "./RTE"
+import postcard from './postcard'
+import PostForm from "./post-form/PostForm";
+import AuthLayout from './AuthLayout'
+import Select from "./Select";
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    postcard,
+    AuthLayout,
+    Select,
+    Input,
+    Button
+
+
+
+}
